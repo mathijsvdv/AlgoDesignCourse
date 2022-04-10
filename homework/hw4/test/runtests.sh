e3a1f35922422c7fedfd3800e1180e4c1e901d70
Mathijs van der Vlies <mathijsvdvlies@solcon.nl>
2022-04-10 20:21:41 +0200

    Use relative paths in runtests.sh

--- homework/hw4/test/runtests.sh
+++ homework/hw4/test/runtests.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # copy source to test directory
 echo "Copying code to test/ directory"
-cp ~/workspace/submit/Arbitrage.java .
-cp ~/workspace/submit/MST.java .
-cp ~/workspace/submit/MyTests.java .
+cp ../submit/Arbitrage.java .
+cp ../submit/MST.java .
+cp ../submit/MyTests.java .
 
 # delete the .class files (if they exist?)
 if [ -e Arbitrage.class ]; then
